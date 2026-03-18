b811536bdf9d86da59ebf8828dacf456e629f88a
Lander <starlink.dis.13510@gmail.com>
2026-03-18 16:33:36 -0700

    New features have been created.

--- src/file_manager/file_manager.py
+++ src/file_manager/file_manager.py
@@ -15,6 +15,11 @@ class FileManager:
     
     @staticmethod
     def _validate_name(name):
+
+        """
+        validation of allowed characters for the name of a folder or file.
+        """
+
         if len(name) > FileManager.MAX_NAME_LENGHT:
             return False, f"Error: Name '{name}' exceeds {FileManager.MAX_NAME_LENGTH} characters."
         if not name.strip():
@@ -22,7 +27,12 @@ class FileManager:
         return True, None
 
     @staticmethod
-    def create_folder(path_name, directory=None):
+    def create_folder(path_name):
+
+        """
+        function for creating folders.
+        """
+
         is_valid, error_msg = FileManager._validate_name(path_name)
         if not is_valid: return error_msg
 
@@ -34,6 +44,11 @@ class FileManager:
         
     @staticmethod
     def create_file(file_name, content=""):
+
+        """
+        function for a creation files.
+        """
+
         is_valid, error_msg = FileManager._validate_name(file_name)
         if not is_valid: return error_msg
 
@@ -91,3 +106,21 @@ class FileManager:
             sub_index = ' ' * 4 * (level + 1)
             for f in files:
                 print(f"{sub_index}📄 {f}")
+
+    @staticmethod            
+    def move_files(source_path, destination_route):
+
+        """
+        function to manage and move files.
+        """
+
+        if not os.path.exists(source_path):
+            raise FileNotFoundError(f"The source file does not exist: {source_path}")
+        
+        directory = os.path.dirname(destination_route)
+
+        if directory and not os.path.exists(directory):
+            os.makedirs(directory)
+
+        os.rename(source_path, destination_route)
+        print(f"File successfully moved to: {destination_route}")
